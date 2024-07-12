33ab5afaa8eb46073b93a92220bb938d91d1fe71
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-07-12 11:16:53 +0300

    Create initial state and actions

---
+++ src/app/states/favourite_product/action/app.action.ts
@@ -0,0 +1,10 @@
+import { createAction, props } from '@ngrx/store';
+import { FavoriteProduct } from '../../models/favourite-product.model';
+
+//take description as first param and data to be processed as second param
+//four actions add,remove,update and clear
+export const add = createAction('[FavoriteProduct] Add',  props<{ product: FavoriteProduct }>());
+export const remove = createAction('[FavoriteProduct] Remove', props<{ product: FavoriteProduct }>());
+export const updateAllState = createAction('[FavoriteProduct] Update all state of favorites products', 
+ props<{ products: FavoriteProduct[] }>());
+export const clear = createAction('[FavoriteProduct] Clear')
---
+++ src/app/states/favourite_product/app.state.ts
@@ -0,0 +1,5 @@
+import { FavoriteProduct } from "../models/favourite-product.model";
+
+export interface AppState {
+    products:FavoriteProduct[];  //initial state product of type favouriteproduct array
+}
