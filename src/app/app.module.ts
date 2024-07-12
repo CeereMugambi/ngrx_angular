ea34ce8fdede7ea609900e517d4e36ef38f2c597
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-07-12 11:29:16 +0300

    Create selector and fetch some data from favourite store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { favoriteReducer } from './states/favourite_product/reducer/app.reducer';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({favourite:favoriteReducer})
   ],
   providers: [
     provideClientHydration()
