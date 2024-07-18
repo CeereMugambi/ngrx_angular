de7cf965d71a124465b0aa717c97dab5c9420c6e
Janet Ceere Mugambi <JANET.MUGAMBI@equitybank.co.ke>
2024-07-18 13:18:38 +0300

    Create home-product component together with services

--- src/app/home/home/home.module.ts
+++ src/app/home/home/home.module.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeProductComponent } from './home-product/home-product.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
+
+
 
 
 @NgModule({
@@ -11,7 +17,10 @@ import { HomeProductComponent } from './home-product/home-product.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule
   ],
   exports:[
     HomeProductComponent
